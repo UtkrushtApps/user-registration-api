23e5b58beb532ba9055382aebdcbd75487116851
Naman Muley <naman.g.muley@gmail.com>
2025-07-29 18:58:12 +0530

    Add app/models/user.py

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    name: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    name: str
